8fb36aa8a02ff47e365c5d234e5974096aaf264c
Awais Niazi <niazi.awais@hotmail.com>
2026-07-17 21:30:55 +0500

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3549810847" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2213953406" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
